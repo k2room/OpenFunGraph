7846ecf97e7de143e311b2e6718d2ba7fa4b759f
k2room <kimyirum@gm.gist.ac.kr>
2025-10-15 13:33:03 +0900

    fix cuda device

--- openfungraph/scenegraph/detection_scenefun3d.sh
+++ openfungraph/scenegraph/detection_scenefun3d.sh
@@ -1,11 +1,13 @@
 # generate object 2D detection
-CUDA_VISIBLE_DEVICES=0 python scripts/generate_gsa_results.py     --dataset_root $SCENEFUN3D_ROOT     --dataset_config $SCENEFUN3D_CONFIG_PATH     --scene_id $SCENE_NAME     --class_set $CLASS_SET     --box_threshold 0.25     --text_threshold 0.25     --stride 1     --add_bg_classes     --accumu_classes     --exp_suffix withbg_allclasses
+# CUDA_VISIBLE_DEVICES=0 
+python scripts/generate_gsa_results.py     --dataset_root $SCENEFUN3D_ROOT     --dataset_config $SCENEFUN3D_CONFIG_PATH     --scene_id $SCENE_NAME     --class_set $CLASS_SET     --box_threshold 0.25     --text_threshold 0.25     --stride 1     --add_bg_classes     --accumu_classes     --exp_suffix withbg_allclasses
 
 # fuse general objects
 python slam/cfslam_pipeline_batch.py     dataset_root=$SCENEFUN3D_ROOT     dataset_config=$SCENEFUN3D_CONFIG_PATH     stride=1     scene_id=$SCENE_NAME     spatial_sim_type=overlap     mask_conf_threshold=0.3     match_method=sim_sum     sim_threshold=${THRESHOLD}     dbscan_eps=0.1     gsa_variant=ram_withbg_allclasses     skip_bg=False     max_bbox_area_ratio=0.9    merge_overlap_thresh=0.9 save_suffix=overlap_maskconf0.3_bbox0.9_simsum${THRESHOLD}_dbscan.1 merge_visual_sim_thresh=0.75 merge_text_sim_thresh=0.7
 
 # detect 2D parts
-CUDA_VISIBLE_DEVICES=0 python scripts/generate_part_gsa_results.py     --dataset_root $SCENEFUN3D_ROOT     --dataset_config $SCENEFUN3D_CONFIG_PATH     --scene_id $SCENE_NAME     --class_set $CLASS_SET     --box_threshold 0.15     --text_threshold 0.15     --stride 1     --add_bg_classes     --accumu_classes     --exp_suffix withbg_allclasses
+# CUDA_VISIBLE_DEVICES=0 
+python scripts/generate_part_gsa_results.py     --dataset_root $SCENEFUN3D_ROOT     --dataset_config $SCENEFUN3D_CONFIG_PATH     --scene_id $SCENE_NAME     --class_set $CLASS_SET     --box_threshold 0.15     --text_threshold 0.15     --stride 1     --add_bg_classes     --accumu_classes     --exp_suffix withbg_allclasses
 
 # fuse parts
 python slam/cfslam_pipeline_batch.py     dataset_root=$SCENEFUN3D_ROOT     dataset_config=$SCENEFUN3D_CONFIG_PATH     stride=1     scene_id=$SCENE_NAME     spatial_sim_type=overlap     mask_conf_threshold=0.15     match_method=sim_sum     sim_threshold=${THRESHOLD}     dbscan_eps=0.1     gsa_variant=ram_withbg_allclasses     skip_bg=False     max_bbox_area_ratio=0.1     save_suffix=overlap_maskconf0.15_bbox0.1_simsum${THRESHOLD}_dbscan.1_parts part_reg=True
